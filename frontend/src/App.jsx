1c496a1a952a504697761c86c91d864460ec9de7
vikkynxe <vigneshnxe@gmail.com>
2026-07-09 23:58:34 +0530

    nothing herer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,23 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/log_in";
 import MusicPlayer from "./pages/MusicPlayer";
 
 function App() {
-  return <MusicPlayer />;
+  const isLoggedIn = localStorage.getItem("isLoggedIn");
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route
+          path="/MusicPlayer"
+          element={
+            isLoggedIn === ("true" || '' || null) ? <MusicPlayer /> : <Navigate to="/" />
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/log_in.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import "./log_in.css";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Email:", email);
+    console.log("Password:", password);
+    localStorage.setItem("isLoggedIn", "true");
+    alert("Login Successful!");
+  };
+
+  return (
+    <div className="login-container">
+      <form className="login-box" onSubmit={handleSubmit}>
+        <h2>Login</h2>
+
+        <div className="input-group">
+          <label>Email</label>
+          <input
+            type="email"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+
+        <div className="input-group">
+          <label>Password</label>
+          <input
+            type="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+
+        <button type="submit">Login</button>
+
+        <p className="signup-text">
+          Don't have an account? <a href="/">Sign Up</a>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
